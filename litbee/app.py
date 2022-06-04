79037e9be967595ed45611be4fe34bfc0b1b3139
freemt <yucongo+fmt@gmail.com>
2022-06-04 11:16:58 +0800

    Update

--- litbee/app.py
+++ litbee/app.py
@@ -55,12 +55,13 @@ from pathlib import Path
 from types import SimpleNamespace
 from typing import Optional
 
-import debee
-import dzbee
-import ezbee
 import loguru
 import logzero
 import pandas as pd
+import ezbee
+import dzbee
+import debee
+
 import streamlit as st
 from loguru import logger as loggu
 from logzero import logger
@@ -68,25 +69,25 @@ from set_loglevel import set_loglevel
 from streamlit import session_state as state
 
 from litbee import __version__
-from litbee.home import home
-from litbee.info import info
-from litbee.multipage import Multipage
-from litbee.settings import settings
+# from litbee.options import options
 
 # from litbee.files2df import files2df
 # from litbee.utils import sb_front_cover, instructions, menu_items
 # from litbee.ezbee_page import ezbee_page
 # from litbee.dzbee_page import dzbee_page
 # from litbee.xbee_page import xbee_page
-from litbee.utils import menu_items, style_css
+from litbee.utils import menu_items
 
-# from litbee.options import options
-
+from litbee.multipage import Multipage
 
 # from litbee.fetch_upload import fetch_upload
 # from litbee.fetch_paste import fetch_paste
 # from litbee.fetch_urls import fetch_urls
 
+from litbee.home import home
+from litbee.settings import settings
+from litbee.info import info
+from litbee.utils import style_css
 
 # from ezbee import ezbee
 
